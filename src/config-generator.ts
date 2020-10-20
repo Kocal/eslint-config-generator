43225fb7e8813b1c64b3c62bf255c2665538e7c2
Hugo Alliaume <kocal@live.fr>
2020-10-20 12:54:59 +0200

    feat(config): add Vue support, close #1

--- src/config-generator.ts
+++ src/config-generator.ts
@@ -1,9 +1,15 @@
-type ESLintConfig = import('eslint').Linter.Config;
+import { normalizeUserOptions, Options, UserOptions } from './options';
 
-export function generateConfig(): ESLintConfig {
+export type ESLintConfig = import('eslint').Linter.Config & {
+  extends?: string[]
+};
+
+export function generateConfig(userOptions: UserOptions = {}): ESLintConfig {
+  const options = normalizeUserOptions(userOptions);
   const config = getBaseConfig();
 
-  applyPrettierConfig(config);
+  applyPrettierConfig(config, options);
+  applyVueConfig(config, options);
 
   return config;
 }
@@ -27,11 +33,33 @@ function getBaseConfig(): ESLintConfig {
   };
 }
 
-function applyPrettierConfig(config: ESLintConfig): void {
+function applyPrettierConfig(config: ESLintConfig, options: Options): void {
   config.extends = (config.extends || []).concat(require.resolve('eslint-config-prettier'));
+  if (options.vue) {
+    config.extends.push(require.resolve('eslint-config-prettier/vue'));
+  }
   config.plugins = (config.plugins || []).concat('prettier');
   config.rules = {
     ...config.rules || {},
     'prettier/prettier': 'error'
+  };
+}
+
+function applyVueConfig(config: ESLintConfig, options: Options): void {
+  if(options.vue === false) {
+    return;
+  }
+
+  const pluginVueConfig = `${options.vue.version === 3 ? 'vue3-' : ''}` + options.vue.config;
+
+  config.extends?.unshift(require.resolve(`eslint-plugin-vue/lib/configs/${pluginVueConfig}`));
+  config.parser = 'vue-eslint-parser';
+  config.rules = {
+    ...config.rules || {},
+    'vue/component-name-in-template-casing': [`error`, `PascalCase`, {
+      'registeredComponentsOnly': false,
+      'ignores': [],
+    }],
+    'vue/html-self-closing': ['error', { html: { normal: 'never', void: 'always' } }],
   };
 }
